a75ac40b528617181a244511121b5353ca53559b
Frame <nafoizret@web.de>
2025-03-09 18:25:01 +0100

    rename WindowsInputDeviceHost -> RawInputDeviceHost

--- source/RawInputDeviceHost.cs
+++ source/RawInputDeviceHost.cs
@@ -5,13 +5,13 @@ using System.Threading;
 
 namespace ChaosFramework.Input.RawInput
 {
-    public class WindowsInputDeviceHost : InputDeviceHost
+    public class RawInputDeviceHost : InputDeviceHost
     {
         static readonly TimeSpan SLEEP_TIME = new TimeSpan(0, 0, 0, 0, 1);
 
         readonly InputCaptureForm form;
 
-        public WindowsInputDeviceHost(InputContext context)
+        public RawInputDeviceHost(InputContext context)
         {
             form = new InputCaptureForm(context);
         }
